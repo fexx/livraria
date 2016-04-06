e97b212fda64ba57cdca3ce32c01580c16d078c2
Fernando <fexx@hotmail.co.uk>
2016-04-06 16:36:01 -0300

    00 - Commit initial

---
+++ src/br/com/caelum/livraria/dao/AutorDao.java
@@ -0,0 +1,37 @@
+package br.com.caelum.livraria.dao;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import br.com.caelum.livraria.modelo.Autor;
+
+@Stateless
+public class AutorDao {
+	
+	@PersistenceContext
+	private EntityManager manager;
+	
+	@PostConstruct
+	private void aposCricao() {
+		System.out.println("AutorDao foi criado");
+
+	}
+
+	public void salva(Autor autor) {
+		manager.persist(autor);
+	}
+	
+	public List<Autor> todosAutores() {
+		return manager.createQuery("select a from Autor a", Autor.class).getResultList();
+	}
+
+	public Autor buscaPelaId(Integer autorId) {
+		Autor autor = this.manager.find(Autor.class, autorId);
+		return autor;
+	}
+	
+}
